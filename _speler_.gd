1f965f669faddf3007200b1685d0375038170de7
Rezamatteo <145553385+Rezamatteo@users.noreply.github.com>
2024-07-05 10:46:46 +0200

    ballenmevrouw

--- _speler_.gd
+++ _speler_.gd
@@ -1,8 +1,17 @@
 extends PathFollow3D
 @onready var play = $play
-
+var secondsElapsed=0
+@onready var time = $time
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("run"):
+		time.stop()
+		play.speed_scale=1/secondsElapsed
 		play.play("progress")
+
+
+func _on_timer_timeout():
+	secondsElapsed+=1
+	time.start()
+	
